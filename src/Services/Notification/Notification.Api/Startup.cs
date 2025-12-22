22b2f1ab85e57e98cdd7accd0b7b95b6175c351d
mmolloja <milton_molloja@hotmail.com>
2025-12-22 00:21:22 -0300

    fix: add missing Microsoft.Extensions.Logging using statement

--- src/Services/Notification/Notification.Api/Startup.cs
+++ src/Services/Notification/Notification.Api/Startup.cs
@@ -19,6 +19,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Notification.Api.Consumers;
 using Notification.Persistence.Database;
--- src/Services/Payment/Payment.Api/Startup.cs
+++ src/Services/Payment/Payment.Api/Startup.cs
@@ -19,6 +19,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Payment.Persistence.Database;
 using Payment.Service.EventHandlers.Handlers;
